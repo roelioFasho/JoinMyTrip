0f7b76e624f0b69d0b4bcbae9f265efe8915010c
Noel Hysa <noel.hysa04@gmail.com>
2026-05-02 17:03:51 +0200

    Added Trips Database

---
+++ Database/Tripstable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Trips (
+    trip_id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    trip_name VARCHAR(100) NOT NULL,
+    time DATETIME,
+    destination VARCHAR(100),
+    itinerary TEXT,
+    cost DECIMAL(10,2)
+);
