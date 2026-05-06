64e047f9058df30a3f7870f9d856733f3cdf7327
Noel Hysa <noel.hysa04@gmail.com>
2026-05-06 21:14:36 +0200

    Trip Controller

--- Controller/TripController.php
+++ Controller/TripController.php
@@ -5,22 +5,37 @@ require_once "../Database/TripRepository.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $photoPath = "uploads/" . $_FILES["trip_photo"]["name"];
-    move_uploaded_file($_FILES["trip_photo"]["tmp_name"], "../" . $photoPath);
+    $imagePath = null;
+
+    if (isset($_FILES["trip_image"]) && $_FILES["trip_image"]["error"] === 0) {
+        $uploadDir = "../uploads/";
+
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+
+        $fileName = time() . "_" . basename($_FILES["trip_image"]["name"]);
+        $imagePath = "uploads/" . $fileName;
+
+        move_uploaded_file($_FILES["trip_image"]["tmp_name"], $uploadDir . $fileName);
+    }
 
     $trip = new Trip(
         null,
-        $_POST['trip_name'],
-        $_POST['time'],
-        $_POST['destination'],
-        $_POST['itinerary'],
-        $_POST['cost'],
-        $photoPath
+        $_POST['trip_name'] ?? '',
+        $_POST['departure'] ?? null,
+        $_POST['return_date'] ?? null,
+        $_POST['destination'] ?? '',
+        $_POST['itinerary'] ?? '',
+        $_POST['cost'] ?? 0,
+        $_POST['category'] ?? '',
+        $imagePath
     );
 
     $repo = new TripRepository($conn);
     $repo->insertTrip($trip);
 
     header("Location: ../View/list_trips.php");
+    exit();
 }
 ?>
