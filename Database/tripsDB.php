2fb0e82c50878ede9d94b01095516d9aaba5652d
Noel Hysa <noel.hysa04@gmail.com>
2026-05-11 11:10:36 +0200

    Update

--- Database/tripsDB.php
+++ Database/tripsDB.php
@@ -1,4 +1,4 @@
 <?php
-$conn = new PDO("mysql:host=localhost;dbname=joinmytrip", "root", "");
+$conn = new PDO("mysql:host=localhost;dbname=projekti", "root", "1234");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
